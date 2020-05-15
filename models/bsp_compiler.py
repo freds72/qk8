e0af85586f858431640415607ec767a8a5b03886
Frederic Souchu <frederic.souchu@ingenico.com>
2020-05-15 15:44:49 +0200

    fixed multi-cart export got DOOM E1M1 running (yeah!)

--- models/bsp_compiler.py
+++ models/bsp_compiler.py
@@ -6,6 +6,22 @@ import random
 import math
 import socket
 
+# file helpers
+local_dir = os.path.dirname(os.path.realpath(__file__))
+pico_dir = ""
+if socket.gethostname()=="FRWS3706":
+    pico_dir = os.path.join("C:",os.path.sep,"pico-8-0.2.0")
+else:
+    pico_dir = os.path.join("D:",os.path.sep,"pico-8_0.1.12c")
+
+def call(args):
+    proc = Popen(args, stdout=PIPE, stderr=PIPE)
+    out, err = proc.communicate()
+    exitcode = proc.returncode
+    #
+    return exitcode, out, err
+
+
 # pack helpers
 def tohex(val, nbits):
     return (hex((int(round(val,0)) + (1<<nbits)) % (1<<nbits))[2:]).zfill(nbits>>2)
@@ -99,6 +115,10 @@ PLANE_THICKNESS=0.001
 # linedef
 class Polygon():
   def __init__(self, v0, v1, extra=None, vertices=None):
+    if v0<0:
+      raise Exception("Invalid vertex id: {}".format(v0))
+    if v1<0:
+      raise Exception("Invalid vertex id: {}".format(v1))
     self.v0 = v0
     self.v1 = v1
     self.vertices = vertices
@@ -265,6 +285,7 @@ def lua_vector(pair):
 def export_bsp_tree(tree, depth):
   root = tree.root
   properties = root.properties
+
   s = '{}{}{}{}{}{}'.format(
     pack_variant(root.v0+1),
     pack_variant(root.v1+1),
@@ -286,6 +307,50 @@ def export_bsp_tree(tree, depth):
 
   s += "{:02X}{}".format(flags, subtree)
   return s
+
+def to_cart(s,cart_name,cart_id):
+    cart="""\
+pico-8 cartridge // http://www.pico-8.com
+version 16
+__lua__
+-- data cart for POOM
+local data="{}"
+local mem=0x3100
+for i=1,#data,2 do
+    poke(mem,tonum("0x"..sub(data,i,i+1)))
+    mem+=1
+end
+cstore()
+"""
+    tmp=s[:2*0x2000]
+    # swap bytes
+    gfx_data = ""
+    for i in range(0,len(tmp),2):
+        gfx_data = gfx_data + tmp[i+1:i+2] + tmp[i:i+1]
+    cart += "__gfx__\n"
+    cart += re.sub("(.{128})", "\\1\n", gfx_data, 0, re.DOTALL)
+
+    map_data=s[2*0x2000:2*0x3000]
+    if len(map_data)>0:
+        cart += "__map__\n"
+        cart += re.sub("(.{256})", "\\1\n", map_data, 0, re.DOTALL)
+
+    gfx_props=s[2*0x3000:2*0x3100]
+    if len(gfx_props)>0:
+        cart += "__gff__\n"
+        cart += re.sub("(.{256})", "\\1\n", gfx_props, 0, re.DOTALL)
+
+    # save cart + export cryptic music+sfx part
+    sfx_data=s[2*0x3100:2*0x4300]
+    cart_path = os.path.join(local_dir, "..", "carts", "{}_{}.p8".format(cart_name,cart_id))
+    f = open(cart_path, "w")
+    f.write(cart.format(sfx_data))
+    f.close()
+
+    # run cart
+    exitcode, out, err = call([os.path.join(pico_dir,"pico8.exe"),"-x",cart_path])
+    if err:
+        raise Exception('Unable to process pico-8 cart: {}. Exception: {}'.format(cart_path,err))
 
 class BSP_Compiler():
   def __init__(self, vertices, lines, sides, sectors):
@@ -311,19 +376,15 @@ class BSP_Compiler():
 
     s += export_bsp_tree(tree, 0)
 
-    # convert to pico binary format
-    cart = ""
-    tmp=s[:2*0x2000]
-    # swap bytes
-    gfx_data = ""
-    for i in range(0,len(tmp),2):
-        gfx_data = gfx_data + tmp[i+1:i+2] + tmp[i:i+1]
-    cart += "__gfx__\n"
-    cart += re.sub("(.{128})", "\\1\n", gfx_data, 0, re.DOTALL)
-
-    map_data=s[2*0x2000:2*0x3000]
-    if len(map_data)>0:
-        cart += "__map__\n"
-        cart += re.sub("(.{256})", "\\1\n", map_data, 0, re.DOTALL)
-    
-    print(cart)
+    cart_id = 0
+    cart_data = ""
+    for b in s:
+        cart_data += b
+        # full cart?
+        if len(cart_data)==2*0x4300:
+            to_cart(cart_data,"poom",cart_id)
+            cart_id += 1
+            cart_data = ""
+    # remaining data?
+    if len(cart_data)!=0:
+        to_cart(cart_data,"poom",cart_id)
